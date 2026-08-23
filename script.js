a79c54bb769e7a8bbcc63748a3f9b4ff42ea8c4b
Ezelopez89 <lopezpe671@gmail.com>
2026-08-23 14:30:19 -0300

    Add function to display a message in script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+function mostrarMensaje() {
 
+    document.getElementById("mensaje").textContent =
+        "Próximamente encontrarás aquí las actividades de Matemática, Lengua, Ciencias Sociales y Ciencias Naturales.";
+
+}
